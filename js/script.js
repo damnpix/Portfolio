eaf085ec59cd2e2698e020ac0cf7c739fd2a355e
damnpix <111631956+damnpix@users.noreply.github.com>
2024-02-04 16:40:52 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+//Load github project page from project block h2
+const link = "https://github.com/damnpix/";
+
+function getName() {
+  let html = this.querySelector('h2').innerHTML;
+  open(link + html);
+}
+
+// Wait for the DOM content to load before accessing elements
+document.addEventListener('DOMContentLoaded', function() {
+  var products = document.getElementsByClassName('project_block');
+  for (let i = 0; i < products.length; i++) {
+    products[i].onclick = getName;
+  }
+});
